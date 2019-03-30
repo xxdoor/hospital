0518b770d09341501b83af2b670bbba666a8290e
xxdoor <tacitljy@163.com>
2019-03-30 22:34:15 +0800

    [modify]相对路径打包

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    css: {
-        loaderOptions: { // 向 CSS 相关的 loader 传递选项
-            less: {
-                javascriptEnabled: true
-            }
-        }
-    }
+    publicPath: '.'
 };
