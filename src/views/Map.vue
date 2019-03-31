5674a8a560d98d882d5ebe213c94dd42e1a423c4
xxdoor <tacitljy@163.com>
2019-03-31 18:51:46 +0800

    [add] search tool

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="map">
-        <baidu-map id="map" :center="center" :zoom="zoom">
+        <baidu-map id="map" :center="center" :zoom="zoom" @ready="handler">
             <bm-marker :position="center" :dragging="true" animation="BMAP_ANIMATION_BOUNCE"
                         :icon="icon">
-                <bm-label content="吕梁怡华妇产医院" :labelStyle="{color: 'deeppink', fontSize : '18px'}" :offset="{width: -35, height: 30}"/>
-                <bm-driving :end="center" :auto-viewport="true"></bm-driving>
             </bm-marker>
             <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true"></bm-geolocation>
         </baidu-map>
+        <div class="tool">
+            <div class="search">
+                <mu-form :model="form" label-position="left" label-width="100">
+                    <mu-form-item label="出发">
+                        <mu-text-field v-model="form.start"></mu-text-field>
+                    </mu-form-item>
+                    <mu-form-item label="到达">
+                        <mu-text-field v-model="form.end"></mu-text-field>
+                    </mu-form-item>
+                </mu-form>
+            </div>
+            <div class="check">
+                <mu-button icon color="deeppink" @click="searchMap">
+                    <mu-icon value="search" size="36"></mu-icon>
+                </mu-button>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -48,6 +64,11 @@
                 lng: 111.13491,
                 lat: 37.511818
             };
+            let form = {
+                start: '当前位置',
+                end: '吕梁怡华妇产医院'
+            };
+
             return {
                 // detail: detail,
                 center: center,
@@ -55,6 +76,94 @@
                 icon: {
                     url: require('../assets/s_hos.png'),
                     size: {width: 30, height: 30},
+                },
+                form: form,
+                map: null,
+                BMap: null
+            }
+        },
+        methods: {
+            handler: function (obj) {
+                console.log("in map handler..");
+                let BMap = obj.BMap;
+                let map = obj.map;
+                //将两个实例赋值给data
+                this.map = map;
+                this.BMap = BMap;
+                let center_point = new BMap.Point(this.center.lng, this.center.lat);
+
+                // 添加文字标签
+                let label_position = {
+                    position: center_point,
+                    offset: new BMap.Size(-42, 10)
+                };
+                let label = new BMap.Label("吕梁怡华妇产医院", label_position);
+                label.setStyle({
+                    color: 'hotpink',
+                    fontSize: '12px',
+                    height: '20px'
+                });
+                map.addOverlay(label);
+
+                // 获取当前位置
+                let current_point;
+                let geolocation = new BMap.Geolocation();
+                geolocation.getCurrentPosition(function (r) {
+                    if (this.getStatus() === 0) {
+                        current_point = r.point;
+                        // 计算当前位置和医院的距离
+                        let distance = map.getDistance(current_point, center_point);
+
+                        if (distance <= 500) {
+                            console.log("步行");
+                            let walking = new BMap.WalkingRoute(map, {
+                                renderOptions: {map: map, autoViewport: true}
+                            });
+                            walking.search(current_point, center_point);
+                        } else {
+                            console.log("开车");
+                            let driving = new BMap.DrivingRoute(map, {
+                                renderOptions: {map: map, autoViewport: true}
+                            });
+                            driving.search(current_point, center_point);
+                        }
+                    } else {
+                        alert("没有定位到您的位置呢> <")
+                    }
+                }, {enableHighAccuracy: true})
+
+            },
+            searchMap: function () {
+                // 检查起点和终点是否改变
+                if (this.form.start === '当前位置' && this.form.end === '吕梁怡华妇产医院') {
+                    alert('位置没有改变，不搜索');
+                } else {
+                    let start = this.form.start;
+                    let end = this.form.end;
+                    let BMap = this.BMap;
+                    let map = this.map;
+
+                    console.log("开车");
+                    let driving = new BMap.DrivingRoute(map, {
+                        renderOptions: {map: map, autoViewport: true}
+                    });
+                    driving.search(start, end);
+                    //TODO 如何计算距离
+                    // let distance = this.map.getDistance(start, end);
+                    //
+                    // if (distance <= 500) {
+                    //     console.log("步行");
+                    //     let walking = new BMap.WalkingRoute(map, {
+                    //         renderOptions: {map: map, autoViewport: true}
+                    //     });
+                    //     walking.search(current_point, center_point);
+                    // } else {
+                    //     console.log("开车");
+                    //     let driving = new BMap.DrivingRoute(map, {
+                    //         renderOptions: {map: map, autoViewport: true}
+                    //     });
+                    //     driving.search(current_point, center_point);
+                    // }
                 }
             }
         }
@@ -65,6 +174,30 @@
 
     #map {
         width: 100%;
-        height: 500px;
+        height: 800px;
     }
+
+    .tool {
+        position: fixed;;
+        bottom: 70px;
+        height: 20%;
+        width: 100%;
+        background-color: white;
+        opacity: 0.5;
+    }
+
+    .search, .check {
+        display: inline-block;
+    }
+
+    .search {
+        width: 80%;
+    }
+    .check {
+        width: 20%;
+    }
+    .mu-form-item {
+        width: 90%;
+    }
+
 </style>
